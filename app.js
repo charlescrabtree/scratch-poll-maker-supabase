236e933d06c0356f21f6d1c6a28cf8343a53670f
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-12 13:05:09 -0700

    initalied state, grabbed form, imported functions

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 // import functions and grab DOM elements
+import { createNewPoll, getPolls } from "./fetch-utils.js";
+import { renderPoll } from "./render-utils.js";
+
+const createForm = document.getElementById('create-poll');
+const voteA = document.getElementById('vote-a');
+const voteB = document.getElementById('vote-b');
+const closePoll = document.getElementById('publish-poll');
 
 // let state
+let question = '';
+let optionA = '';
+let optionB = '';
+let optionAVotes = 0;
+let optionBVotes = 0;
 
 // set event listeners 
   // get user input
